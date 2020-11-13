9de02960b40f2eb2ae1dd8b053182141bae3f4b3
tmdbts <DV9344**>
2020-11-13 08:52:51 +0000

    step-2 begining

--- src/org/academiadecodigo/javabank/application/operations/ViewBalance.java
+++ src/org/academiadecodigo/javabank/application/operations/ViewBalance.java
@@ -1,10 +1,8 @@
 package org.academiadecodigo.javabank.application.operations;
 
-import org.academiadecodigo.javabank.domain.account.Account;
-
 public class ViewBalance implements Operation {
     @Override
     public void execute() {
-        
+
     }
 }
---
+++ src/org/academiadecodigo/javabank/views/LoginView.java
@@ -0,0 +1,16 @@
+package org.academiadecodigo.javabank.views;
+
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.integer.IntegerInputScanner;
+
+public class LoginView {
+
+    public void start(){
+
+    }
+
+    Prompt prompt = new Prompt(System.in, System.out);
+
+    IntegerInputScanner integerInputScanner = new IntegerInputScanner();
+
+}
