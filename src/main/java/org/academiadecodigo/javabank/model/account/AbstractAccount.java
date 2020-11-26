2dddff89ac73d84764bd036518116f4495018a88
tmdbts <DV9344**>
2020-11-26 16:42:10 +0000

    added documentation

--- src/main/java/org/academiadecodigo/javabank/model/account/AbstractAccount.java
+++ src/main/java/org/academiadecodigo/javabank/model/account/AbstractAccount.java
@@ -38,26 +38,56 @@ public abstract class AbstractAccount extends AbstractModel implements Account {
      */
     public abstract AccountType getAccountType();
 
+    /**
+     * Gets the version on the DataBase
+     *
+     * @return version on the DataBase
+     */
     public Integer getVersion() {
         return version;
     }
 
+    /**
+     * Sets the version on the DataBase
+     *
+     * @param version the version on the DataBase
+     */
     public void setVersion(Integer version) {
         this.version = version;
     }
 
+    /**
+     * Gets the date of creation of the account
+     *
+     * @return the data of creation of the account
+     */
     public Date getCreationTime() {
         return creationTime;
     }
 
+    /**
+     * Sets the creation time of the account
+     *
+     * @param creationTime the creation time of the account
+     */
     public void setCreationTime(Date creationTime) {
         this.creationTime = creationTime;
     }
 
+    /**
+     * Gets the last time that the account was updated
+     *
+     * @return the last time that the account was updated
+     */
     public Date getLastUpdate() {
         return lastUpdate;
     }
 
+    /**
+     * Sets the last time that the account was updated
+     *
+     * @param lastUpdate the last time that the account was updated
+     */
     public void setLastUpdate(Date lastUpdate) {
         this.lastUpdate = lastUpdate;
     }
