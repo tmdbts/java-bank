51edb1679fbe5f5b3aec9ee5efe2784062132f6c
tmdbts <DV9344**>
2020-12-01 11:07:31 +0000

    Added new classes and interfaces for DAO implementations

---
+++ src/main/java/org/academiadecodigo/javabank/persistence/TransactionManager.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.javabank.persistence;
+
+public interface TransactionManager {
+}
---
+++ src/main/java/org/academiadecodigo/javabank/persistence/jpa/JpaSessionManager.java
@@ -0,0 +1,4 @@
+package org.academiadecodigo.javabank.persistence.jpa;
+
+public class JpaSessionManager {
+}
